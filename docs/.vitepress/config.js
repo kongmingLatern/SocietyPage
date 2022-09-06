f2ced7ef5ab26067ea1826dcdd88d6619781cd13
凤之兮原 <1577310596@qq.com>
2022-09-06 22:17:03 +0800

    update: update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,9 +4,10 @@ export default {
   themeConfig: {
     sidebar: [
       {
-        text: '社团介绍',
+        text: '社团',
         items: [
-          { text: '介绍', link: '/home/introduction' },
+          { text: '社团介绍', link: '/home/introduction' },
+          { text: '社团活动', link: '/active/school' },
         ]
       },
       {
@@ -14,12 +15,6 @@ export default {
         items: [
           { text: '校级奖项', link: '/award/school' },
           { text: '院级奖项', link: '/award/collage' },
-        ]
-      },
-      {
-        text: '社团活动',
-        items: [
-          { text: '学校比赛', link: '/active/school' },
         ]
       },
       {
