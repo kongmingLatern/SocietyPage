de0baa20c61cc792815296d7e01f6bdd852bc1cb
凤之兮原 <1577310596@qq.com>
2022-09-06 22:07:24 +0800

    feat: add themeConfig

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,27 @@
 export default {
   title: '网络空间安全社团',
-  description: 'For The Feture'
+  description: 'For The Feture',
+  themeConfig: {
+    sidebar: [
+      {
+        text: '社团介绍',
+        items: [
+          { text: '介绍', link: '/home/introduction' },
+        ]
+      },
+      {
+        text: '社团成就',
+        items: [
+          { text: '校级奖项', link: '/item-c' },
+          { text: '院级奖项', link: '/item-d' },
+        ]
+      },
+      {
+        text: '加入我们',
+        items: [
+          { text: 'QQ', link: '/item-a' },
+        ]
+      },
+    ]
+  }
 }
